4f96b28db82cc6d13e3deb099cba3c54e8ab53cd
김종만 <v666v@naver.com>
2024-01-23 23:20:12 +0900

    20240123

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
+  const name = "리액트";
+  const flag = "N";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.!!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>리액트야</h1>
+      <h2>잘작동하니</h2>
+      <h3>내 이름은 {name}이야</h3>
+      {flag === "Y" ? <h1> flag : Y! </h1> : <h1>flag : not Y!</h1>}
+    </>
   );
 }
 
